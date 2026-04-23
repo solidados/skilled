24914b8ff484776df003e991adbcb0607075e9b4
solidados <geekbrains.pk@internet.ru>
2026-04-23 17:38:37 +0400

    chore: minor changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,24 @@
-import { Link } from '@tanstack/react-router';
-import { LogIn } from 'lucide-react';
+import { Link } from "@tanstack/react-router";
+import { LogIn } from "lucide-react";
 
 const NavBar = () => (
-  <nav className="navbar">
-    <div className="brand">
-      <div className="mark">
-        <div className="glyph" />
-      </div>
-      <Link to="/">
-        <span>Skild</span>
-      </Link>
-    </div>
-    
-    <div className="actions">
-      <Link to="/sign-in/$" className="btn-primary">
-        <LogIn size={16}/>
-        Sign in
-      </Link>
-    </div>
-  </nav>
+	<nav className="navbar">
+		<div className="brand">
+			<div className="mark">
+				<div className="glyph" />
+			</div>
+			<Link to="/">
+				<span>Skild</span>
+			</Link>
+		</div>
+
+		<div className="actions">
+			<Link to="/sign-in/$" className="btn-primary">
+				<LogIn size={16} />
+				Sign in
+			</Link>
+		</div>
+	</nav>
 );
 
 export default NavBar;
